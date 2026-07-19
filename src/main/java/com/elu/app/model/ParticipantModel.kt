156e4c69c6862821955e6915b2c620dd869041b8
Kabir <skaal1399@gmail.com>
2026-07-19 22:07:33 +0530

    Delete src/main directory

--- src/main/java/com/elu/app/model/ParticipantModel.kt
+++
@@ -1,9 +0,0 @@
-package com.elu.app.model
-
-data class ParticipantModel(
-    val uid: String = "",
-    val name: String = "",
-    val avatar: String = "",
-    val seatIndex: Int = -1, // -1 means not sitting
-    val isHost: Boolean = false
-)
